66856107315fb00db091e869de084972b7fc436b
Aliseiko <aliseiko.dev@gmail.com>
2022-01-15 02:08:24 +0300

    feat: add audioplayer and download button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+const audio = document.querySelector('audio'),
+    playButton = document.querySelector('.player__play-pause-button'),
+    downloadButton = document.querySelector('.player__download-button');
+let isPlay = false;
+
+
 function removeActiveClass(fromClass) {
     Array.from(document.querySelectorAll(`.${fromClass}`)).find(elem => elem.classList.contains('active')).classList.remove('active');
 }
@@ -20,7 +26,46 @@ function changeImg(bird) {
 
 document.querySelector('.nav__list').addEventListener('click', (event) => {
     if (event.target.closest('.nav__item')) {
-        markActiveMenuItem(event.target.closest('.nav__item').dataset.bird);
-        changeImg(event.target.closest('.nav__item').dataset.bird);
+        const currentBird = event.target.closest('.nav__item').dataset.bird;
+        markActiveMenuItem(currentBird);
+        setAudioSrc(currentBird);
+        changeImg(currentBird);
+        if (isPlay) playAudio();
+        setDownloadLink(currentBird);
     }
 })
+
+// -------------- audio-player ---------------------------------
+
+function playAudio() {
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+}
+
+function pauseAudio() {
+    audio.pause();
+    isPlay = false;
+}
+
+function setAudioSrc(bird) {
+    audio.src = `assets/audio/${bird}.mp3`;
+}
+
+function setActivePlayButton() {
+    Array.from(document.querySelectorAll(`.player__play-pause-button-img`)).find(elem => elem.classList.contains('active-button')).classList.remove('active-button');
+    document.querySelector(`.${(isPlay) ? 'pause-button' : 'play-button'}`).classList.add('active-button');
+}
+
+playButton.addEventListener('click', (event) => {
+    if (event.target.closest('.player__play-pause-button')) {
+        (isPlay) ? pauseAudio() : playAudio();
+        setActivePlayButton();
+    }
+})
+
+// ----------------- download sound -----------------------
+
+function setDownloadLink(bird) {
+    document.querySelector('.download-link').href = `assets/audio/${bird}.mp3`;
+}
